6a24f8dc862042690b4e5f8896844804afb9b66d
NandhiniMB-Accolite <nandhini.mb@accolitedigital.com>
2021-03-07 14:39:23 +0530

    take assessment

--- assessment/src/main/java/com/ms/assessment/Entity/MCQ.java
+++ assessment/src/main/java/com/ms/assessment/Entity/MCQ.java
@@ -1,16 +1,15 @@
 package com.ms.assessment.Entity;
 
 import java.util.Date;
-import java.util.Set;
+import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -31,8 +30,8 @@ public class MCQ {
 	@ManyToOne
 	private Course course;
 	
-	@OneToMany(cascade = CascadeType.ALL,mappedBy="mcq")
-	private Set<Question> questions;
+	@ManyToMany
+	private List<User> participants;
 	
 	public Course getCourse() {
 		return course;
