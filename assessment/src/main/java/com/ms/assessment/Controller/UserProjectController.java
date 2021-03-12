b6a6a6d5fc4103491d8f91589501123386bc167c
NandhiniMB-Accolite <nandhini.mb@accolitedigital.com>
2021-03-12 11:33:15 +0530

    Bug Fixes

--- assessment/src/main/java/com/ms/assessment/Controller/UserProjectController.java
+++ assessment/src/main/java/com/ms/assessment/Controller/UserProjectController.java
@@ -6,8 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -49,7 +49,7 @@ public class UserProjectController {
 	
 	
 	@PostMapping("/score")
-	public User_Project saveUserProjectScore(User_Project up) {
+	public User_Project saveUserProjectScore(@RequestBody User_Project up) {
 		return userProjectService.saveUserProject(up);
 	}
 }
